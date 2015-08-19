879718bc6c5cf93b04dc03c30e8aca7acbdd8022
Brandon Powell <bpowell29a@gmail.com>
2015-08-19 12:47:18 -0400

    added basic game stucts

--- game/game.go
+++ game/game.go
@@ -1,4 +1,21 @@
 package game
 
-func DoNothing() {
+type Hero struct {
+	Damage int
+	Level  int
+}
+
+func NewHero() *Hero {
+	return &Hero{}
+}
+
+type Player struct {
+	Hero1    *Hero
+	Hero2    *Hero
+	Hero3    *Hero
+	Currency int
+}
+
+func NewPlayer() *Player {
+	return &Player{}
 }
